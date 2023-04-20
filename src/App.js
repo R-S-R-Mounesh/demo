d3b24c48d6ce7f447e09ef07e86214e919b9d309
R-S-R-Mounesh <rsrmounesh@gmail.com>
2023-04-20 14:00:57 +0530

    My React Project

---
+++ src/App.js
@@ -0,0 +1,59 @@
+//import logo from "./logo.svg";
+
+import "./App.css";
+import React from "react";
+import DemoWeb from "./Components/WebsiteDemo";
+
+
+//import AppDemo from "./Components/reactexadd";
+//import ListComponent from "./Components/ListCmp";
+//npmimport AppAdd from "./Components/FormAdd";
+
+//import ControlledComponent from "./Components/Controlled";
+//import TableDemo from "./Components/TableComponent";
+//import ListComponent from "./Components/ListCmp";
+//import UncontrolledCompnent from "./Components/Uncontrolled";
+
+//import LifeCycle from "./Components/cmpLifeCycle";
+//import CounterMain from "./Components/Counter";
+
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div>
+    {/*<CounterMain />*/}
+    
+    {/*<LifeCycle  favBike ="Rx"/>*/}
+    
+    {/*<UncontrolledCompnent />*/}
+  {/*<ControlledComponent />*/}
+  {/*<TableDemo />*/}
+  {/*<ListComponent />*/}
+ <DemoWeb />
+  </div>
+  );
+  }
+
+// class Count extends React.Component (){
+   // render() {
+    //  return(
+      //  <div>
+      // <h3>FirstComponent</h3>
+      // </div>
+    //  )
+//}
+ // }
+
+
+//Counter.propTypes = {
+ // inc:propTypes.number
+//}
+   
+  
+export default App;
